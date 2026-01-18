5d0562b5a449f7832fdc9d6dbb5320cea214511f
sharanyabhiram <sharanyabhiram@gmail.com>
2026-01-18 19:55:01 +0530

    DateTime code

---
+++ Javascript/DisplayDateAndTime.js
@@ -0,0 +1,9 @@
+var today = new Date();
+var date = today.getDate();
+var day = today.getDay();
+
+console.log(date);
+
+var daylist = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+console.log("day: "+daylist[day]);
